02cec4fc0718a0ef39200e88b0d6a25b44c8ba2a
Miguel Aguirre <md.aguim@gmail.com>
2018-03-19 16:12:31 -0300

    Dynamic pages set up done

--- config.rb
+++ config.rb
@@ -20,3 +20,10 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+# Proxies for dynamic pages :
+
+["papillard", "ssaunier", "monsieurpaillard"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+
+end
